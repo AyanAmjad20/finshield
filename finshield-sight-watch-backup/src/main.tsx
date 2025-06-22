34a8a3e2218b0cf283c6dd70c1fbd8e97969b7e5
Ayan Amjad <ayanamjad20@gmail.com>
2025-06-22 11:33:38 -0400

    AA

--- finshield-sight-watch-backup/src/main.tsx
+++ finshield-sight-watch-backup/src/main.tsx
@@ -1,11 +1,5 @@
-// src/main.tsx
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/finshield">
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")!).render(<App />);
